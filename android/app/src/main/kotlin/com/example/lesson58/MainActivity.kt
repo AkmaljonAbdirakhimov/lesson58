b938868897b0d420becb886721fbb21cc6ce54bc
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-06-13 12:36:24 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/lesson58/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lesson58
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  final String id;
+  String title;
+
+  Product({
+    required this.id,
+    required this.title,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+    };
+  }
+}
